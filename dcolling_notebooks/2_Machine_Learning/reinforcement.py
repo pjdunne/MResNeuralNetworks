8414ff9eaa2ebb5b5b71f1fe12fcd1bc3724294f
Chaoyi Jing <jingchaoyi@gmail.com>
2023-08-02 21:37:10 +0100

    Update reinforcement.py

--- dcolling_notebooks/2_Machine_Learning/reinforcement.py
+++ dcolling_notebooks/2_Machine_Learning/reinforcement.py
@@ -218,43 +218,49 @@ game_loop(num_episodes)
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def users_choice():
     print("Let's play rock, paper, scissors")
-    # choice = random.choice(options)
-    choice = "rock"
+    choice = random.choice(options)
+    # choice = "rock"
     return choice
 
 
 options = ["rock", "paper", "scissors"]
 
+document_result = []
+
 
 def get_reward(user_choice, comp_choice):
     if user_choice == comp_choice:
+        document_result.append("draw")
         return 0  # Draw
     elif (
         (user_choice == "rock" and comp_choice == "scissors")
         or (user_choice == "scissors" and comp_choice == "paper")
         or (user_choice == "paper" and comp_choice == "rock")
     ):
-        return 1  # Comp Lose
+        document_result.append("win")
+        return 5  # Comp Lose
     else:
-        return -1  # Comp Win
+        document_result.append("lose")
+        return -2  # Comp Win
 
 
-num_episodes = 50
+num_episodes = 1000
 
 # the learning rate, Just like in supervised learning settings
-alpha = 0.2
+alpha = 0.9
 # discount rate, closer to 1 captures long-term effective reward
-gamma = 0.8
+gamma = 0.7
 # Instead of just selecting the best learned Q-value action,
 # we'll sometimes favor exploring the action space further.
 # Lower epsilon value results in episodes with more penalties
 # (on average) which is obvious because we are exploring and
 # making random decisions.
-epsilon = 0.5
+epsilon = 0.2
 
 q_table = {}
 options = ["rock", "paper", "scissors"]
@@ -300,8 +306,6 @@ list_of_comps_input = []
 
 
 def get_state(user_choice, comp_choice):
-    list_of_users_input.append(user_choice)
-    list_of_comps_input.append(comp_choice)
     return (user_choice, comp_choice)
 
 
@@ -315,6 +319,8 @@ def game_loop(num_episodes):
         reward, state = play_game(state)
         total_reward += reward
         contin_reward.append(total_reward)
+        list_of_users_input.append(users_choice())
+        list_of_comps_input.append(play_game(state)[1])
 
     # visualise reward
     average_reward = total_reward / num_episodes
@@ -323,6 +329,7 @@ def game_loop(num_episodes):
 
 game_loop(num_episodes)
 
+### visualising results
 x_var = np.linspace(0, num_episodes, num_episodes)
 plt.plot(x_var, contin_reward)
 plt.title("Reward Score Build-UP")
@@ -330,4 +337,49 @@ plt.xlabel("Number of Iterations")
 plt.ylabel("Reward")
 plt.show()
 
+record = pd.DataFrame(
+    {"Player's Choices": list_of_users_input, "Computer's Choices": list_of_comps_input}
+)
+
+slice_result = record.iloc[-10:, :]
+print(slice_result)
+
+wins = document_result[document_result["result"] == "win"]
+losses = document_result[document_result["result"] == "lose"]
+draws = document_result[document_result["result"] == "draw"]
+
+
+# %%
+import pandas as pd
+
+document_result = ["win", "lose", "draw", "win", "lose", "draw", "win", "lose", "draw"]
+document_result = pd.DataFrame({"result": document_result})
+
+
+wins = document_result[document_result["result"] == "win"]
+losses = document_result[document_result["result"] == "lose"]
+draws = document_result[document_result["result"] == "draw"]
+
+# for i in document_result["result"]:
+#     if i == "win":
+#         slice = document_result[document_result["result"] == "win"]
+#         wins.append(slice)
+
+print(wins)
+# %%
+import pandas as pd
+import numpy as np
+
+list_of_users_input = np.array([1, 2, 5])
+list_of_comps_input = np.array([3, 4, 6])
+
+
+record = pd.DataFrame(
+    {"Player's Choices": list_of_users_input, "Computer's Choices": list_of_comps_input}
+)
+print(record)
+# print(record.shape[0])
+slice_result = record.iloc[-10:, :]
+
+print(slice_result)
 # %%
